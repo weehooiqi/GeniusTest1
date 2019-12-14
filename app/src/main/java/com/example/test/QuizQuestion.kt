ba4f7a5a6e9091b8d64d6fca562b27d98630c5bd
abkec <davidkor1999@gmail.com>
2019-12-15 01:34:57 +0800

    commit

--- app/src/main/java/com/example/test/QuizQuestion.kt
+++ app/src/main/java/com/example/test/QuizQuestion.kt
@@ -1,14 +1,19 @@
 package com.example.test
 
 import android.app.Activity
+import android.app.AlertDialog
 import android.graphics.Color
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.databinding.DataBindingUtil
-import androidx.navigation.findNavController
+
 import com.example.test.databinding.QuizQuestionBinding
+
+
+
 
 class QuizQuestion : AppCompatActivity() {
 
@@ -42,9 +47,8 @@ class QuizQuestion : AppCompatActivity() {
     lateinit var currentQuestion: Question
     lateinit var answers: MutableList<String>
     private var questionIndex = 0
-    private val numQuestions = Math.min((questions.size + 1) / 2, 3)
+    private val numQuestions = questions.size
     private lateinit var binding: QuizQuestionBinding
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -54,57 +58,64 @@ class QuizQuestion : AppCompatActivity() {
         val toolbar = findViewById<View>(R.id.toolbar2) as Toolbar
         setSupportActionBar(toolbar)
 
+        getSupportActionBar()?.setHomeAsUpIndicator(R.drawable.ic_close_24px);
         getSupportActionBar()?.setDisplayHomeAsUpEnabled(true)
         getSupportActionBar()?.setDisplayShowHomeEnabled(true)
-        getSupportActionBar()?.setTitle("Question 1")
+
         toolbar.setBackgroundColor(Color.parseColor("#ff0000"))
-
-
-
-
 
         randomizeQuestions()
 
         // Bind this fragment class to the layout
         binding.game = this
 
+        var check = true
 
         // Set the onClickListener for the submitButton
         binding.submitButton.setOnClickListener @Suppress("UNUSED_ANONYMOUS_PARAMETER")
         { view: View ->
-            val checkedId = binding.questionRadioGroup.checkedRadioButtonId
-            // Do nothing if nothing is checked (id == -1)
-            if (-1 != checkedId) {
-                var answerIndex = 0
-                when (checkedId) {
-                    R.id.secondAnswerRadioButton -> answerIndex = 1
-                    R.id.thirdAnswerRadioButton -> answerIndex = 2
-                    R.id.fourthAnswerRadioButton -> answerIndex = 3
-                }
-                // The first answer in the original question is always the correct one, so if our
-                // answer matches, we have the correct answer.
-                if (answers[answerIndex] == currentQuestion.answers[0]) {
-                    questionIndex++
-                    // Advance to the next question
-                    if (questionIndex < numQuestions) {
-                        currentQuestion = questions[questionIndex]
-                        setQuestion()
-                        binding.invalidateAll()
+
+
+
+                val checkedId = binding.questionRadioGroup.checkedRadioButtonId
+                // Do nothing if nothing is checked (id == -1)
+                if (-1 != checkedId) {
+                    var answerIndex = 0
+                    when (checkedId) {
+                        R.id.secondAnswerRadioButton -> answerIndex = 1
+                        R.id.thirdAnswerRadioButton -> answerIndex = 2
+                        R.id.fourthAnswerRadioButton -> answerIndex = 3
+                    }
+                    // The first answer in the original question is always the correct one, so if our
+                    // answer matches, we have the correct answer.
+                    if (answers[answerIndex] == currentQuestion.answers[0]) {
+                        questionIndex++
+                        // Advance to the next question
+                        if (questionIndex < numQuestions) {
+                            currentQuestion = questions[questionIndex]
+                            setQuestion()
+                            binding.invalidateAll()
+                        } else {
+                            Toast.makeText(applicationContext,"Quiz Completed.",Toast.LENGTH_SHORT).show()
+                            super.onBackPressed()
+                        }
                     } else {
-                        // We've won!  Navigate to the gameWonFragment.
+                        Toast.makeText(applicationContext,"The answer is " + currentQuestion.answers[0],Toast.LENGTH_SHORT).show()
+                        
 
                     }
-                } else {
-                    // Game over! A wrong answer sends us to the gameOverFragment.
-
                 }
-            }
+
+
+
+
         }
 
 
     }
 
     override fun onSupportNavigateUp(): Boolean {
+
         onBackPressed()
         return true
     }
@@ -119,6 +130,35 @@ class QuizQuestion : AppCompatActivity() {
         currentQuestion = questions[questionIndex]
         answers = currentQuestion.answers.toMutableList()
         answers.shuffle()
+        getSupportActionBar()?.setTitle("Question " + String.format("%d",questionIndex+1))
+    }
 
+    override fun onBackPressed() {
+
+        val builder = AlertDialog.Builder(this@QuizQuestion, R.style.AlertDialogCustom)
+
+        // Set the alert dialog title
+        builder.setTitle("Exit?")
+
+        // Display a message on alert dialog
+        builder.setMessage("The quiz progress will be lost!")
+
+        // Set a positive button and its click listener on alert dialog
+        builder.setPositiveButton("YES"){dialog, which ->
+            // Do something when user press the positive button
+            Toast.makeText(applicationContext,"You have quited.",Toast.LENGTH_SHORT).show()
+            super.onBackPressed()
+        }
+
+        // Display a neutral button on alert dialog
+        builder.setNeutralButton("Cancel"){_,_ ->
+
+        }
+
+        // Finally, make the alert dialog using builder
+        val dialog: AlertDialog = builder.create()
+
+        // Display the alert dialog on app interface
+        dialog.show()
     }
 }
--- app/src/main/java/com/example/test/ui/quiz/QuizFragment.kt
+++ app/src/main/java/com/example/test/ui/quiz/QuizFragment.kt
@@ -1,17 +1,21 @@
 package com.example.test.ui.quiz
 
+import android.app.AlertDialog
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.example.test.QuizQuestion
 import com.example.test.R
 import kotlinx.android.synthetic.main.fragment_quiz.view.*
+
 
 class QuizFragment : Fragment() {
 
@@ -27,10 +31,26 @@ class QuizFragment : Fragment() {
 
         val view = inflater.inflate(R.layout.fragment_quiz, container, false)
 
+        val b = view.q1 as Button
+        val buttonText = b.text.toString()
+
         view.q1.setOnClickListener {
 
-            val intent = Intent(activity, QuizQuestion::class.java)
-            startActivity(intent)
+            val builder = AlertDialog.Builder(activity, R.style.AlertDialogCustom)
+
+            builder.setTitle("Quiz Time")
+            builder.setMessage("Start $buttonText?")
+            builder.setPositiveButton("YES"){dialog, which ->
+                val intent = Intent(activity, QuizQuestion::class.java)
+                startActivity(intent)
+                Toast.makeText(activity,"Start!", Toast.LENGTH_SHORT).show()
+            }
+            builder.setNeutralButton("Cancel"){_,_ ->
+
+            }
+
+            val dialog: AlertDialog = builder.create()
+            dialog.show()
         }
 
         return view
